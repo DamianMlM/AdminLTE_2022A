1f2f5c97e000040029f220ff519284dc9424ea39
DamianMlM <leonel.mariscal5040@alumnos.udg.mx>
2022-06-24 00:12:24 -0500

    Plugins y cambios en el index raiz

--- layouts/header_index.php
+++ layouts/header_index.php
@@ -9,30 +9,31 @@
             <!-- Google Font: Source Sans Pro -->
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
         <!-- Font Awesome -->
-        <link rel="stylesheet" href="../../plugins/fontawesome-free/css/all.min.css">
+        <link rel="stylesheet" href="./plugins/fontawesome-free/css/all.min.css">
         <!-- Ionicons -->
         <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
         <!-- Tempusdominus Bootstrap 4 -->
-        <link rel="stylesheet" href="../../plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
+        <link rel="stylesheet" href="./plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
         <!-- iCheck -->
-        <link rel="stylesheet" href="../../plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+        <link rel="stylesheet" href="./plugins/icheck-bootstrap/icheck-bootstrap.min.css">
         <!-- JQVMap -->
-        <link rel="stylesheet" href="../../plugins/jqvmap/jqvmap.min.css">
+        <link rel="stylesheet" href="./plugins/jqvmap/jqvmap.min.css">
         <!-- Theme style -->
-        <link rel="stylesheet" href="../../dist/css/adminlte.min.css">
+        <link rel="stylesheet" href="./dist/css/adminlte.min.css">
         <!-- overlayScrollbars -->
-        <link rel="stylesheet" href="../../plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
+        <link rel="stylesheet" href="./plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
         <!-- Daterange picker -->
-        <link rel="stylesheet" href="../../plugins/daterangepicker/daterangepicker.css">
+        <link rel="stylesheet" href="./plugins/daterangepicker/daterangepicker.css">
         <!-- summernote -->
-        <link rel="stylesheet" href="../../plugins/summernote/summernote-bs4.min.css">
+        <link rel="stylesheet" href="./plugins/summernote/summernote-bs4.min.css">
+        
         </head>
         <body class="hold-transition sidebar-mini layout-fixed">
             <div class="wrapper">
        
              <!-- Preloader -->
             <div class="preloader flex-column justify-content-center align-items-center">
-                <img class="animation__shake" src="../../dist/img/AdminLTELogo.png" alt="AdminLTELogo" height="60" width="60">
+                <img class="animation__shake" src="./dist/img/AdminLTELogo.png" alt="AdminLTELogo" height="60" width="60">
             </div>
 
                     <!-- Navbar -->
@@ -96,8 +97,8 @@
             <!-- Main Sidebar Container -->
         <aside class="main-sidebar sidebar-dark-primary elevation-4">
             <!-- Brand Logo -->
-            <a href="../../index.php" class="brand-link">
-            <img src="../../dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+            <a href="./index.php" class="brand-link">
+            <img src="./dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
             <span class="brand-text font-weight-light">Ddmin DMlrsd</span>
             </a>
 
@@ -106,7 +107,7 @@
             <!-- Sidebar user panel (optional) -->
             <div class="user-panel mt-3 pb-3 mb-3 d-flex">
                 <div class="image">
-                <img src="../../dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+                <img src="./dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
                 </div>
                 <div class="info">
                 <a href="#" class="d-block">Leonel Damian</a>
@@ -129,13 +130,13 @@
                   </a>
                   <ul class="nav nav-treeview">
                     <li class="nav-item">
-                      <a href="../../views/users/index.php" class="nav-link  ">
+                      <a href="./views/users/index.php" class="nav-link  ">
                         <i class="far fa-circle nav-icon"></i>
                         <p>Listado</p>
                       </a>
                     </li>
                     <li class="nav-item">
-                      <a href="../../views/users/create.php" class="nav-link">
+                      <a href="./views/users/create.php" class="nav-link">
                         <i class="far fa-circle nav-icon"></i>
                         <p>Crear</p>
                       </a>
